39d4100131fed29e0d2c2e33125307ec22189ec6
Jerson M <jerson.mari@gmail.com>
2019-05-07 00:42:51 +0800

    Edit last line

--- application.py
+++ application.py
@@ -76,5 +76,5 @@ def message(data):
 	emit("new message", {"user": data["user"], "dateString": data["date"], "timeString": data["time"], "message": data["message"]}, room=data["room"])
 
 
-if __name__ == '__main__':
-    socketio.run(app, port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+	socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
